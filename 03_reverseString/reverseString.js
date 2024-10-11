250a286f273a566d9604cabb8898a8bf4fdcbdf8
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-10-11 13:28:23 +0330

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (myStr) {
+	let revStr = "";
+	for (let i = myStr.length - 1; i >= 0; i--) {
+		revStr += myStr[i];
+	}
+	return revStr;
 };
 
 // Do not edit below this line
