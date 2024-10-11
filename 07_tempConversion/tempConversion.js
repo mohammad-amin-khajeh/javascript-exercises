7bfceacc4fd686f528e2e61ace39d1ec5d75dc4a
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-10-11 18:44:05 +0330

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+	const celcius = (fahrenheit - 32) * (5 / 9);
+	return +celcius.toFixed(1);
+  // I coul've just used:
+  // return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+	const fehrenheit = celcius * (9 / 5) + 32;
+	return +fehrenheit.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
