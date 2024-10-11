65b3081ae76e251affaf62e3c51673bcbaf57da9
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-10-11 13:40:29 +0330

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+	for (const arg of args) {
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] !== arg) continue;
+			arr.splice(i, 1);
+			// since we removed one element from the array we have to decrease the index by 1
+			// otherwise we'd skip one which is undesirable
+			i--;
+		}
+	}
+	return arr;
 };
 
 // Do not edit below this line
