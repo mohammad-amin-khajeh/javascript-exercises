8651d2383b8a5f3753cc1b12aba9a3000d70ca8e
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-10-11 13:49:39 +0330

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (start > 0 || typeof start !== 'number' || Math.ceil(start) !== start) return "ERROR"
+  if (end > 0 || typeof end !== 'number' || Math.ceil(end) !== end) return "ERROR"
+
+  if (start > end) [start,end] = [end,start]
+
+  let sum = 0
+  for (let i = start; i <= end; i++) {
+    sum += i
+  }
+  return sum
 
 };
 
