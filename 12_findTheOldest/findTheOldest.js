8b67b8e03b65f6834f185e3e4794e846561b571d
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-10-17 18:00:45 +0330

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
+  return people.reduce((oldest, current) => (oldest.yearOfDeath ?? currentYear) - oldest.yearOfBirth > (current.yearOfDeath ?? currentYear) - current.yearOfBirth ? oldest : current , 0);
 };
 
 // Do not edit below this line
