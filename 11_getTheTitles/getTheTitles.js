0f5b87960a54c7f8f84e6fbeee9ef45723428ab4
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-10-17 17:04:10 +0330

    Solve exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+	// my solution
+	// return books.reduce((total, current) => total.concat(current.title), []);
 
+	// a more concise solution
+	return books.map((book) => book.title);
 };
 
 // Do not edit below this line
