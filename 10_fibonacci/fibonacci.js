bff7dba06aff5587ff5382ed082bf39e9a3d9621
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-10-17 17:04:02 +0330

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+	n = +n;
+	if (n < 0) return "OOPS";
 
+	if (!n) return 0;
+
+	let first = 1,
+		second = 0;
+
+	for (let _ = 2; _ <= n; _++) [first, second] = [first + second, first];
+
+	return first;
 };
 
 // Do not edit below this line
