619c7664da296a876c07ed27ee1e7224ba983fa6
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-10-17 16:27:13 +0330

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	return arr.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	return Array(b)
+		.fill(a)
+		.reduce((a, b) => a * b, 1);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+	let arr = [];
+	for (let i = 1; i <= n; i++) arr.push(i);
+	return multiply(arr);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
+
+console.log(sum([4, 6]));
