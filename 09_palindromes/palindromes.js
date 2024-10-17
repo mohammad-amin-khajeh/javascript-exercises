0991af539db6e0b55e66380b05c5a8b539fc7eb7
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-10-17 17:03:47 +0330

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string
+    .replaceAll(/\W/g, "")
+    .toLowerCase();
 
+  let l = 0, r = string.length - 1
+  while (l < r) {
+    if (string[l] !== string[r]) return false
+    l++
+    r--
+  }
+  return true
 };
 
 // Do not edit below this line
